96ce848bc7abd45f4321b0e6f3fb3e46c8f044b7
chtty <631486004@qq.com>
2022-10-10 08:15:04 +0800

    feat: 架构思维

--- docs/leetCode/index.js
+++ docs/leetCode/index.js
@@ -1,24 +1,5 @@
-var swapPairs = function(head) {
-  if (head == null || head.next == null) return { head };
-  const next = head.next;
-  head.next = swapPairs(next.next);
-  next.next = head;
-  return next;
-};
-
-var swapPairs = function(head) {
-  const pre = new ListNode();
-  pre.next = head;
-  let temp = pre;
-
-  while (temp.next !== null && temp.next.next !== null) {
-    let first = temp.next;
-    let second = temp.next.next;
-    // 从头开始翻转
-    temp.next = second;
-    first.next = second.next;
-    second.next = first;
-    temp = first;
+var hasCycle = function(head) {
+  if (head === null) {
+    return false;
   }
-  return pre.next;
 };
