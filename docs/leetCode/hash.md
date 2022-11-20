7fc99b03a507425810a87cd9ab2141e2245fc53e
chtty <631486004@qq.com>
2022-11-21 07:32:03 +0800

    feat: 算法题库

--- docs/leetCode/hash.md
+++ docs/leetCode/hash.md
@@ -1,7 +1,7 @@
 # hash 表、映射、集合
-
+
 https://leetcode-cn.com/problems/valid-anagram/description/
-
+
 https://leetcode-cn.com/problems/group-anagrams/
-
+
 https://leetcode-cn.com/problems/two-sum/description/
---
+++ docs/leetCode/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} senate
+ * @return {string}
+ */
+var predictPartyVictory = function(senate) {
+  let stack = [];
+  let i = 0;
+  while (i < senate.length) {
+    if (senate[i] === "R") {
+      senate.push(senate[i]);
+    } else {
+    }
+  }
+};
