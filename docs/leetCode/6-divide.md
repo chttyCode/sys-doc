49720046ee1932c41f20888274888e0003bba725
chtty <631486004@qq.com>
2022-12-26 07:25:39 +0800

    feat: 算法目录

---
+++ docs/leetCode/6-divide.md
@@ -0,0 +1,4 @@
+# 分子&回溯
+
+[50. Pow(x, n)](https://leetcode.cn/problems/powx-n/)
+[78. 子集](https://leetcode.cn/problems/subsets/)
--- docs/leetCode/index.js
+++ docs/leetCode/index.js
@@ -1,14 +1,26 @@
-/**
- * @param {string} senate
- * @return {string}
- */
-var predictPartyVictory = function(senate) {
-  let stack = [];
-  let i = 0;
-  while (i < senate.length) {
-    if (senate[i] === "R") {
-      senate.push(senate[i]);
+function sort(nums) {
+  sort_c(nums, 0, nums.length - 1);
+}
+function sort_c(nums, s, n) {
+  if (s >= n) {
+    return nums;
+  }
+  const q = (s + n) / 2;
+  sort_c(nums, 0, q - 1);
+  sort_c(nums, q + 1, n);
+  merge(nums.slice(s, n), nums.slice(0, q), nums.slice(q + 1, n));
+}
+
+function merge(nums, nums1, nums2) {
+  let j = nums2.length - 1;
+  let len = nums.length - 1;
+  while (j >= 0) {
+    if (nums2[j] > nums1[i]) {
+      nums1[len] = nums2[j--];
     } else {
+      nums1[len] = nums1[i--];
     }
   }
-};
+}
+const nums = [3, 0, 1];
+console.log(sort(nums));
